01a827f25c18cf4a9aad32fea667491907c33bc0
dinurk <dinur.kashapov@gmail.com>
2025-02-03 02:05:17 +0500

    Some commit

--- src/modules/pictures/controllers/pictures.controller.ts
+++ src/modules/pictures/controllers/pictures.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { PicturesService } from '../services/pictures.service';
 import { PictureDto } from '../dto/picture.dto';
 import { PictureCreateDto } from '../dto/picture-create.dto';
@@ -12,17 +20,17 @@ export class PicturesController {
     return this.picturesService.findAll();
   }
 
-  @Post('create')
+  @Post()
   async create(@Body() picture: PictureCreateDto): Promise<PictureDto> {
     return this.picturesService.create(picture);
   }
 
-  @Post('update')
+  @Put()
   async update(@Body() picture: PictureDto): Promise<PictureDto> {
     return this.picturesService.update(picture);
   }
 
-  @Post('delete/:id')
+  @Delete('/:id')
   async delete(@Param('id') pictureId: string): Promise<void> {
     return this.picturesService.delete(pictureId);
   }
